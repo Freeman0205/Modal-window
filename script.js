adc50688328a47d73e76c95cca7be8772d554e01
Freeman0205 <61151465+Freeman0205@users.noreply.github.com>
2020-08-28 10:53:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,24 @@
  
  // Modal
 
- let more = document.querySelector('.more'),
- btnDescr = document.querySelectorAll('.description-btn'),
- overlay = document.querySelector('.overlay'),
- close = document.querySelector('.popup-close');
+ let more = document.querySelector('.more'),                   // get button with class '.more'
+ btnDescr = document.querySelectorAll('.description-btn'),    // get a pseudo-array tabs
+ overlay = document.querySelector('.overlay'),               // get modal -  class '.overlay'
+ close = document.querySelector('.popup-close');             // get cover element(X) - class '.popup-close'  
 
-more.addEventListener('click', function() {
+more.addEventListener('click', function() {                //when you click on the button(class '.more') appears modal
  overlay.style.display = 'block';
  this.classList.add('more-splash');
  document.body.style.overflow = 'hidden';
 });
-btnDescr.forEach(function(item) {
+btnDescr.forEach(function(item) {                         // in cycle (forEach) use addEvenetListener for each tabs 
  item.addEventListener('click', function() {
      overlay.style.display = 'block';
      this.classList.add('more-splash');
      document.body.style.overflow = 'hidden';
  });
 });
-close.addEventListener('click', function() {
+close.addEventListener('click', function() {             //when you click on the element(X) (class '.popup-close')  modal closes
  overlay.style.display = 'none';
  more.classList.remove('more-splash');
  document.body.style.overflow = '';
